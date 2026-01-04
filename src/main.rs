6771ba753ed5dfdfb0e829b84222920f0eaae949
dapod93 <davidliois50@gmail.com>
2026-01-04 21:01:54 +0700

    chore: update files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use actix_web::{App, HttpServer, middleware::Logger};
+use env_logger::Env;
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    HttpServer::new(|| App::new().wrap(Logger::default()))
+        .bind(("0.0.0.0", 5000))?
+        .run()
+        .await
 }
